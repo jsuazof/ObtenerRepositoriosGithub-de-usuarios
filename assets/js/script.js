377df52fb58786242f8cced6d28a7610316e2a72
Jorge <93343250+jsuazof@users.noreply.github.com>
2022-02-11 19:31:38 -0300

    update
    update

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("form")
+
+form.addEventListener("submit" , (e) =>{
+    e.preventDefault();
+    let user = document.getElementById("nombre").value 
+    let sinEspacios = user.split(' ').join('')
+    alert (sinEspacios)
+
+    fetch(`https://api.github.com/users/${sinEspacios}`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
+
+})
